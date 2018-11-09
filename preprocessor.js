21ebf9b2514da3690145d7c070c9e86cfdf212b1
nocturnal <foxnezz@gmail.com>
2018-11-10 01:31:58 +0300

    added construction parsing

--- preprocessor.js
+++ preprocessor.js
@@ -1,13 +1,62 @@
+let defaultInput =
+`
+
+3 2
+
+0 0 0 0 0
+0.5 0 1 1 1
+1 0 0 0 0
+
+0 1
+1 2
+
+`
+
 $(document).ready(() =>
 {
     let canvas = document.getElementById("canvas")
     window.ctx = canvas.getContext("2d")
+    $("#input").val(defaultInput.trim())
+    update()
 })
 
 parseConstruction = (raw) =>
 {
-    let lines = raw.replace("\r", "").split("\n")
+    let numbers = raw.replace(/\n|\r/g, " ").split(" ").filter(x => x.length > 0)
     let construction = {}
+
+    let cur = 0
+    getNextNumber = () => numbers[cur++]
+
+    construction.nodeCount = parseInt(getNextNumber())
+    construction.rodCount = parseInt(getNextNumber())
+
+    construction.nodes = []
+    for (let i = 0; i < construction.nodeCount; ++i)
+    {
+        let node = {}
+
+        node.x = parseFloat(getNextNumber())
+        node.y = parseFloat(getNextNumber())
+
+        node.xPermit = parseInt(getNextNumber())
+        node.yPermit = parseInt(getNextNumber())
+        node.zPermit = parseInt(getNextNumber())
+
+        construction.nodes.push(node)
+    }
+
+    construction.rods = []
+    for (let i = 0; i < construction.rodCount; ++i)
+    {
+        let rod = {}
+
+        rod.startNode = parseInt(getNextNumber())
+        rod.endNode = parseInt(getNextNumber())
+
+        construction.rods.push(rod)
+    }
+
     return construction
 }
 
